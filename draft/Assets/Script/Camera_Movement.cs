4fa7fc995505465ec8b0e170d5cb0d037717e864
Pedrofflm <pedro.ffl.m@gmail.com>
2017-02-28 13:01:07 +0100

    real commit

---
+++ draft/Assets/Script/Camera_Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Movement : MonoBehaviour {
+
+    public Vector3 center;
+    
+	// Use this for initialization
+	void Start () {
+        GameObject st = GameObject.FindGameObjectsWithTag("Stage")[0];
+        center = st.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(center);
+    }
+}
