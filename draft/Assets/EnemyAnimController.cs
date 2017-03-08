37f1aa18aaaa754c3318c758253b979e597a2432
kik3tion <kikesalle65@gmail.com>
2017-03-08 14:18:52 +0100

    FollowingBucleSuccess

--- draft/Assets/EnemyAnimController.cs
+++ draft/Assets/EnemyAnimController.cs
@@ -14,49 +14,39 @@ public class EnemyAnimController : MonoBehaviour {
     void Start () {
         player = GameObject.FindGameObjectsWithTag("Player")[0];
         d_enemy = GameObject.Find("Dummy_anim");
-        //newRotation = new Vector3(player.transform.eulerAngles.x, player.transform.eulerAngles.y / 2, player.transform.eulerAngles.z);
-        //d_enemy.transform.eulerAngles = newRotation;
 
     }
 
     // Update is called once per frame
     void Update () {
        
-       //Angle in which the plane is 
+       //Player and enemy angles
         p_angle = player.transform.eulerAngles.y;
         e_angle = d_enemy.transform.eulerAngles.y;
-        //print(e_angle);
-        //Dummy
-        // newRotation.z = player.transform.rotation.y;
-        // d_enemy.transform.Rotate(newRotation);
-        //d_enemy.transform.rotation = 
-        //d_enemy.transform.Rotate(Vector3.up*rotAngle);
-
-        //newRotation = new Vector3(player.transform.eulerAngles.x, player.transform.eulerAngles.y / 2, player.transform.eulerAngles.z);
-        //d_enemy.transform.eulerAngles = newRotation;
-        
+     
+        //To follow the player through the scenario
+        // Use deltaAngle to now which enemy_angle_animation use and trigger attacks
         if (e_angle != p_angle)
         {
-           // print("Looking for player");
-            if (e_angle < p_angle)
+          
+            if (Mathf.DeltaAngle(p_angle, e_angle) > 0)
             {
-                d_enemy.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, d_enemy.transform.eulerAngles.y - 0.3f, player.transform.eulerAngles.z);
-              //  print("-decrease");
+                d_enemy.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, d_enemy.transform.eulerAngles.y - 0.1f, player.transform.eulerAngles.z);
             }
-            if(e_angle > p_angle)
+            if(Mathf.DeltaAngle(p_angle, e_angle) < 0)
              {
-                d_enemy.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, d_enemy.transform.eulerAngles.y +0.3f, player.transform.eulerAngles.z);
-                print("+increase");
+                d_enemy.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, d_enemy.transform.eulerAngles.y +0.1f, player.transform.eulerAngles.z);
             }
-            // d_enemy.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, d_enemy.transform.eulerAngles.y +0.3f , player.transform.eulerAngles.z);
-            //print("DENTRO IF");
+           
         }
         else
         {
-            print("Player Found");
+            print("ATTACK");
+            // Use delta angle to say when the player is in range to trigger attacks
         }
         print("Player angle:"+p_angle);
         print("Enemy angle:"+ e_angle);
+        print("Diference between angles is:"+Mathf.DeltaAngle(p_angle, e_angle));
 
 
     }
