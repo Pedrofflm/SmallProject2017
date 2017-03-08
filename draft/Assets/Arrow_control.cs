764d693ee0303b9aed150e3b15e9f7c60064ebb8
Pedrofflm <pedro.ffl.m@gmail.com>
2017-03-08 13:53:14 +0100

    Arrow control script
    point with arrow from anywhere on the screen. Calculate the power of the
    shot according to the distance covered byu the arrow.
    Resets values on firing (mouseup)

---
+++ draft/Assets/Arrow_control.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow_control : MonoBehaviour {
+    public float angle = 0.0f;
+    public GameObject arrow;
+    // Vector3 v3 = Vector3.zero;
+    public int power = 0;
+    public bool resetPose = false;
+    
+    private Vector2 middle, firstPosition;
+    private bool mouseDown = false;
+    //CursorLockMode wantedMode;
+
+    void Start () {
+        middle = new Vector2(Screen.width / 2, Screen.height / 2);
+        if(!arrow)  arrow= GameObject.FindGameObjectsWithTag("Arrow")[0];
+        Screen.fullScreen = false;
+        Cursor.visible = false;
+        //Cursor.lockState = wantedMode;
+        // arrow.transform.eulerAngles = v3;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Cancel")) { 
+            print("escape");
+            Screen.fullScreen = !Screen.fullScreen;
+        }
+        if (Input.GetButton("Fire1")) {//being dragged
+            if (!mouseDown) {
+                mouseDown = true;
+                firstPosition = Input.mousePosition;
+                print("position"+firstPosition);
+            }
+            
+            
+            float d=Input.mousePosition.x - firstPosition.x;//middle.x
+            if (d == 0) d = 0.1f;
+            float h=Input.mousePosition.y- firstPosition.y;//middle.y
+            float distance_percent = Vector2.Distance(Input.mousePosition, firstPosition)/middle.y;//distance to center would be middle in the second parameter
+            if (distance_percent > 0.3)
+            {
+                power = 1;
+                if (distance_percent > 0.7)
+                {
+                    power=2;
+                    if (distance_percent > 0.9) { power=3; }
+                }
+            }
+            print(power);
+            angle = Mathf.Rad2Deg * Mathf.Atan(h/d);
+            if (d < 0) angle -= 180;
+            arrow.transform.localEulerAngles = new Vector3(arrow.transform.rotation.x, arrow.transform.rotation.y, angle+180);// Quaternion.Euler(arrow.transform.rotation.x, arrow.transform.rotation.y, angle);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //fire arrow projectile
+            mouseDown = false;
+            firstPosition = new Vector2();
+            print("up");
+            power = 0;
+            print(power);
+            if(resetPose)arrow.transform.localEulerAngles = new Vector3(arrow.transform.rotation.x, arrow.transform.rotation.y, 45f);
+            
+        }
+
+    }
+   
+    
+}
