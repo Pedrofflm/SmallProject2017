c400088576bc6a3127b1de462176818fea1307d5
kik3tion <kikesalle65@gmail.com>
2017-04-07 20:19:35 +0200

    Attacks_working

---
+++ draft/Assets/DamageDealer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour {
+    public int weakness = 1;
+   
+   // ParticleSystem particle;
+    Player_State ps;
+    // Use this for initialization
+    void Start()
+    {
+        ps = GameObject.FindGameObjectWithTag("GameEngine").GetComponent<Player_State>();
+        //particle = GetComponentInChildren<ParticleSystem>();
+        //particle.Stop(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("Got Hit by The MONSTAHHH");
+          
+            if (other.GetComponent<Arrow_Power>().power == weaknessType)
+            {
+                ps.decHP(weakness);
+                print("HIIITTT!!!! - " + ps.getHP());
+               // particle.Stop(false);
+               // particle.Play(false);
+            }
+            else { }
+           // Destroy(other.gameObject);
+        }
+    }
+}
--- draft/Assets/Script/EnemyAnimController.cs
+++ draft/Assets/Script/EnemyAnimController.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class EnemyAnimController : MonoBehaviour
 {
-
+    public Vector3 center;
     public float p_speed = 5.0f;
     public float e_speed = 0.1f;
-    public float random_num;
-    GameObject player, d_enemy, d_attack;
+    public float random_num, e_att_speed, dyn_att_speed,sp_att1, sp_att2;
+    GameObject player, d_enemy, d_attack,enemy_ai;
     public float e_angle, p_angle;
     public int attackstate = 2;
     Vector3 newRotation = new Vector3(0, 0, 0);
-
+    float init_pos_at;
     public float cooldownDef, cooldownCur;
     public int attperformance = 0;
+    bool at_dir = false;
     // Use this for initialization
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        d_enemy = GameObject.Find("Dummy_anim");
+        d_enemy = GameObject.FindGameObjectWithTag("enemy_ai");
         d_attack = GameObject.FindGameObjectWithTag("dummy_attack");
+       // enemy_ai = GameObject.FindGameObjectWithTag("enemy_ai");
         cooldownCur = 3;
         cooldownDef = 0;
+        init_pos_at = d_attack.transform.position.y;
+        center = new Vector3(0, init_pos_at, 0);
 
     }
 
@@ -39,25 +43,107 @@ public class EnemyAnimController : MonoBehaviour
         switch (attackstate)
         {
             case 1:
-                if (cooldownCur < 1 && Mathf.DeltaAngle(p_angle, e_angle) >= -5 && Mathf.DeltaAngle(p_angle, e_angle) <= 5)
+                if (cooldownCur > 1 && Mathf.DeltaAngle(p_angle, e_angle) >= -5 && Mathf.DeltaAngle(p_angle, e_angle) <= 5)
                 {
 
                     //Which attack is being performed?
                     switch (attperformance)
                     {
                         case 1:
-                            print("Kablammmm!!!!!!!!");
-                            attackstate = 2;
+                            //Static Attack
+                            sp_att1 = e_att_speed + 1;
+                            if (at_dir == false)
+                            {
+                                if (d_attack.transform.position.y > 0.7f)
+                                {
+                                    // Moving the club downwards
+                                    d_attack.transform.Translate(0f, 0f, sp_att1 * Time.deltaTime);
+                                }
+                                else
+                                {
+                                    at_dir = true;
+                                }
+                            }else {
+
+                                if (d_attack.transform.position.y < init_pos_at)
+                                {
+                                    //Moving the club upwards
+                                    d_attack.transform.Translate(0f, 0f, -e_att_speed * Time.deltaTime);
+                                }
+                                else
+                                {
+                                    attackstate = 2;
+                                    print("Back to searching");
+                                    at_dir = false;
+                                }
+                            }
+
                             break;
 
                         case 2:
-                            print("KASHWWWIIISSHHHHH!!!!!");
-                            attackstate = 2;
+                            //Dynamic Attack
+                            sp_att2 = e_att_speed;
+                            if (at_dir == false)
+                            {
+                                //// Moving the club downwards
+                                if (d_attack.transform.position.y > 0.7f)
+                                {
+                                    //Depending on the position of player it does attack dyagonally to right or left
+                                    if (Mathf.DeltaAngle(p_angle, e_angle) > 0)
+                                    {
+                                        print("Following Right");
+                                        d_attack.transform.Translate(0f, 0f, sp_att2 * Time.deltaTime);
+                                        d_attack.transform.RotateAround(center, Vector3.up, -dyn_att_speed * Time.deltaTime);
+
+                                    }
+                                    if (Mathf.DeltaAngle(p_angle, e_angle) < 0)
+                                    {
+                                        print("Following Left");
+                                        d_attack.transform.Translate(0f, 0f, sp_att2 * Time.deltaTime);
+                                        d_attack.transform.RotateAround(center, Vector3.up, +dyn_att_speed * Time.deltaTime);
+                                    }
+                                    
+                                   
+                                }
+                                else
+                                {
+                                    at_dir = true;
+                                }
+                            }
+                            else {
+
+                                if (d_attack.transform.position.y < init_pos_at)
+                                {
+                                    //Moving the club upwards
+                                    if (Mathf.DeltaAngle(p_angle, e_angle) > 0)
+                                    {
+                                        
+                                        d_attack.transform.Translate(0f, 0f, -sp_att2 * Time.deltaTime);
+                                        d_attack.transform.RotateAround(center, Vector3.up, dyn_att_speed * Time.deltaTime);
+
+                                    }
+                                    if (Mathf.DeltaAngle(p_angle, e_angle) < 0)
+                                    {
+                                        
+                                        d_attack.transform.Translate(0f, 0f, -sp_att2 * Time.deltaTime);
+                                        d_attack.transform.RotateAround(center, Vector3.up, -dyn_att_speed * Time.deltaTime);
+                                    }
+
+                                }
+                                else
+                                {
+                                    attackstate = 2;
+                                    print("Back to searching");
+                                    at_dir = false;
+                                }
+                            }
+
                             break;
 
                         case 3:
                             print("Ain't nobody got time fo dat!!!");
                             attackstate = 2;
+
                             break;
                     }
                 }
@@ -69,7 +155,7 @@ public class EnemyAnimController : MonoBehaviour
                 //Player and enemy angles
                 p_angle = player.transform.eulerAngles.y;
                 e_angle = d_enemy.transform.eulerAngles.y;
-                print("Searchingg");
+               // print("Searchingg");
                 //I cooldown is over
                 if (cooldownCur < 1)
                 {
@@ -95,16 +181,23 @@ public class EnemyAnimController : MonoBehaviour
                         //It is, select the attack. Random to decide IA
                         if (random_num > 0.2)
                         {
-                            attperformance = 1;
+                            //1
+                            attperformance = 2;
                             if (random_num > 0.7)
                             {
                                 attperformance = 2;
                                 if (random_num > 0.8)
                                 {
-                                    attperformance = 3;
+                                    //3
+                                    attperformance = 2;
                                 }
                             }
                         }
+                        else
+                        {
+                            attperformance = 1;
+                        }
+
                         //Attack selection
                         switch (attperformance)
                         {
